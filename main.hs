6a7d79becc447feb765bd1fa120705f3b226fc50
Rodrigo <rodcjoao@gmail.com>
2025-01-10 13:56:21 -0300

    Hamming_Int 2.0

--- main.hs
+++ main.hs
@@ -1,6 +1,11 @@
-hamming :: (Eq a) => [a] -> [a] -> Int -> Int
+hammingaux :: (Eq a) => [a] -> [a] -> Int -> Int
+hamming :: (Eq a) => [a] -> [a] -> Int
 
-hamming [] [] cont = cont
-hamming (x:xs) (y:ys) cont
-    | x /= y = hamming xs ys (cont + 1)
-    | x == y = hamming xs ys cont
+hammingaux [] [] cont = cont
+hammingaux (xs) []  cont = cont + length xs
+hammingaux  [] (ys) cont = cont + length ys
+hammingaux (x:xs) (y:ys) cont
+    | x /= y = hammingaux xs ys (cont + 1)
+    | x == y = hammingaux xs ys cont
+
+hamming (xs) (ys) = hammingaux xs ys 0
